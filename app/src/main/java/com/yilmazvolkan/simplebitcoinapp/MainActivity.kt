26b46d720444dda37f2f36575401d616d1d49a65
yilmazvolkan <volkan.yilmazboun@gmail.com>
2020-12-05 16:59:14 +0300

    Added graph to fragment and fragment connected to activity

--- app/src/main/java/com/yilmazvolkan/simplebitcoinapp/MainActivity.kt
+++ app/src/main/java/com/yilmazvolkan/simplebitcoinapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.yilmazvolkan.simplebitcoinapp
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.yilmazvolkan.simplebitcoinapp.fragments.ShowGraphFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var showGraphFragment: ShowGraphFragment? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+        if (savedInstanceState == null) {
+            startFragment()
+        }
+    }
+
+    private fun startFragment() {
+        showGraphFragment = ShowGraphFragment.newInstance()
+        if (showGraphFragment != null && this.isFinishing.not()) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragment_container, showGraphFragment!!)
+                    .addToBackStack(null)
+                    .commitAllowingStateLoss()
+        }
     }
 }
--- app/src/main/java/com/yilmazvolkan/simplebitcoinapp/fragments/ShowGraphFragment.kt
+++ app/src/main/java/com/yilmazvolkan/simplebitcoinapp/fragments/ShowGraphFragment.kt
@@ -5,8 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import com.yilmazvolkan.simplebitcoinapp.R
 import com.yilmazvolkan.simplebitcoinapp.databinding.FragmentShowGraphBinding
+import com.yilmazvolkan.simplebitcoinapp.ui.CustomMarker
 import com.yilmazvolkan.simplebitcoinapp.util.inflate
 
 class ShowGraphFragment : Fragment() {
@@ -23,5 +27,63 @@ class ShowGraphFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val entries = ArrayList<Entry>()
+
+
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+        entries.add(Entry(6f, 10f))
+        entries.add(Entry(7f, 2f))
+        entries.add(Entry(8f, 7f))
+        entries.add(Entry(9f, 20f))
+        entries.add(Entry(10f, 16f))
+
+
+        entries.add(Entry(11f, 10f))
+        entries.add(Entry(12f, 2f))
+        entries.add(Entry(13f, 7f))
+        entries.add(Entry(14f, 20f))
+        entries.add(Entry(15f, 16f))
+
+
+        val vl = LineDataSet(entries, "My Type")
+
+        vl.setDrawValues(false)
+        vl.setDrawFilled(true)
+        vl.lineWidth = 2f
+        vl.fillColor = R.color.teal_700
+        vl.fillAlpha = R.color.purple_500
+
+
+        binding.lineChart.xAxis.labelRotationAngle = 0f
+
+
+        binding.lineChart.data = LineData(vl)
+
+
+        binding.lineChart.axisRight.isEnabled = false
+        binding.lineChart.xAxis.axisMaximum = 30+0.1f
+
+
+        binding.lineChart.setTouchEnabled(true)
+        binding.lineChart.setPinchZoom(true)
+
+
+        binding.lineChart.description.text = "Days"
+        binding.lineChart.setNoDataText("No bitcoin data yet!")
+
+
+        val markerView = CustomMarker(requireContext(), R.layout.marker_view)
+        binding.lineChart.marker = markerView
+    }
+
+    companion object {
+        fun newInstance(): ShowGraphFragment {
+            return ShowGraphFragment()
+        }
     }
 }
